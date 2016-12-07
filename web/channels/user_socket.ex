e68ae931b06f88d6e20bd7c0f49105b660c4b3d7
shiro105 <shiro105@mail.chapman.edu>
2016-12-07 15:18:47 -0800

    Use production config from Heroku ENV variables and decrease socket timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ClassChat.UserSocket do
   channel "rooms:*", ClassChat.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
@@ -47,7 +48,4 @@ defmodule ClassChat.UserSocket do
   # Returning `nil` makes this socket anonymous.
   # def id(_socket), do: nil
 
-  ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket,
-    timeout: 45_000
 end
